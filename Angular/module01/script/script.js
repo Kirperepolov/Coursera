60b7455c1fd193342933c899a209c5a76d2327ec
Kir Perepolov <johnyd@bigmir.net>
2016-12-05 15:19:06 +0200

    Module 1 bonus
    BONUS (OPTIONAL AND NOT GRADED) task is completed

--- Angular/module01/script/script.js
+++ Angular/module01/script/script.js
@@ -9,33 +9,69 @@
     $scope.lunchList;
     $scope.message;
     $scope.caution;
+    $scope.style;
+    var messages = {
+      ok:{text:'Enjoy!',style:'ok'},
+      notOk:{text:'Too much!',style:'ok'},
+      empty:{text:'Please enter data first',style:'worry'}
+    };
 
+    /**
+     * .checkLunch - this method is called to check whether the entered list of
+     *  lunch dishes is appropriate
+     *
+     * @return {undefined}  the function changes tha values of
+     *                      ng-model variables and returns nothing
+     */
     $scope.checkLunch = function(){
       if($scope.lunchList) {
         var lunchCautionMessage = lunchCaution($scope.lunchList);
         var checkLunchMessage = lunchMessage($scope.lunchList);
       } else {
-        var checkLunchMessage = 'Please enter data first';
+        var checkLunchMessage = messages.empty;
       };
       $scope.caution = lunchCautionMessage;
-      $scope.message = checkLunchMessage;
+      $scope.message = checkLunchMessage.text;
+      $scope.style = checkLunchMessage.style;
     };
 
+    /**
+     * lunchCaution - checks whether a user enters empty items, which
+     * are not considered in counting. If empty items appear
+     * the caution message is displayed
+     *
+     * @param  {string} string an entered list of user's lunch dishes
+     * @return {string}        a caution message. It is only returned when
+     *  user enters empty items
+     */
     function lunchCaution(string){
       string = string.replace(/ /gi,'');
       if (/,,/.test(string)) {
-        var caution = 'Please don\'t enter empty data';
+        var caution = 'Empty items, i.e., ",," are not considered';
       };
       return caution;
     };
 
+    /**
+     * lunchMessage - checks the xact number of dishes a user enters
+     *
+     * @param  {string} string an entered list of user's lunch dishes
+     * @return {object}        an object with a message and message.style
+     *                       depending on number of user's dishes
+     */
     function lunchMessage(string){
+      //as the spaces doesn't influence I decided to delete all of them
+      // to make the next handlinh easier
+      string = string.replace(/ /gi,'');
       var lunchListToArray = string.split(',');
+      //by the .filter I discard empty inputs
       let filteredLunchList = lunchListToArray.filter(substr=>substr!=='');
-      if (filteredLunchList.length<4) {
-        var message = 'Enjoy!';
+      if (filteredLunchList.length===0){
+        var message = messages.empty;
+      } else if (filteredLunchList.length<4) {
+        var message = messages.ok;
       } else {
-        var message = 'Too much!';
+        var message = messages.notOk;
       };
       return message;
     };
