4078e61862f0edf84029bb9f0ea8adbd583afbdf
Kir Perepolov <johnyd@bigmir.net>
2016-12-05 13:42:08 +0200

    Module 1 graded task finished

--- Angular/module01/script/script.js
+++ Angular/module01/script/script.js
@@ -1,44 +1,44 @@
-/*
-(function(){
-'use strict';
-
-  angular.module('nameCalculator',[])
-
-  .controller('NameCalculatorController', function($scope){
-    $scope.name = '';
-    $scope.totalValue = 0;
-
-    $scope.displayNumeric = function () {
-      var totalNameValue = calculateNumericForString($scope.name);
-      $scope.totalValue = totalNameValue;
-    };
-
-    function calculateNumericForString(string){
-      var totalStringValue = 0;
-      for (var i=0;i<string.length;i++){
-        totalStringValue += string.charCodeAt(i);
-      };
-
-      return totalStringValue;
-    };
-  })
-
-})();
-
-*/
 (function(){
   'use strict';
 
-  angular.module('DIApp',[])
-  .controller('DIController', DIController);
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController', LunchCheckController);
 
-  function DIController($scope, $filter){
-    $scope.name = "john";
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope){
+    $scope.lunchList;
+    $scope.message;
+    $scope.caution;
 
-    $scope.upper = function (){
-      var upCase = $filter('uppercase');
-      $scope.name = upCase($scope.name);
+    $scope.checkLunch = function(){
+      if($scope.lunchList) {
+        var lunchCautionMessage = lunchCaution($scope.lunchList);
+        var checkLunchMessage = lunchMessage($scope.lunchList);
+      } else {
+        var checkLunchMessage = 'Please enter data first';
+      };
+      $scope.caution = lunchCautionMessage;
+      $scope.message = checkLunchMessage;
     };
-  }
+
+    function lunchCaution(string){
+      string = string.replace(/ /gi,'');
+      if (/,,/.test(string)) {
+        var caution = 'Please don\'t enter empty data';
+      };
+      return caution;
+    };
+
+    function lunchMessage(string){
+      var lunchListToArray = string.split(',');
+      let filteredLunchList = lunchListToArray.filter(substr=>substr!=='');
+      if (filteredLunchList.length<4) {
+        var message = 'Enjoy!';
+      } else {
+        var message = 'Too much!';
+      };
+      return message;
+    };
+  };
 
 })();
