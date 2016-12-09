da0822fa4007e4d8c211928b6443011d3d2c2c1d
Kir Perepolov <johnyd@bigmir.net>
2016-12-09 15:30:31 +0200

    AngularJS module02 copmleted

--- Angular/module02/script/script.js
+++ Angular/module02/script/script.js
@@ -6,18 +6,83 @@
   .controller('AlreadyBoughtController',AlreadyBoughtController)
   .service("ShoppingListCheckOffService",ShoppingListCheckOffService)
 
+
+  /**
+   * ToBuyController - this controller i supposed for for the toBuyList managing
+   *
+   * @param  {function} ShoppingListCheckOffService a singleton service for
+   *  data storing and sharing
+   *
+   */
+
   ToBuyController.$inject=['ShoppingListCheckOffService'];
   function ToBuyController(ShoppingListCheckOffService){
     var list = this;
-    list.items=[];
-    
+    list.items=ShoppingListCheckOffService.getItemsToBuy();
+
+    list.buyItem = function(itemIndex){
+      ShoppingListCheckOffService.buyItem(itemIndex);
+    };
+
+
   };
 
+  /**
+   * AlreadyBoughtController - this controller i supposed for for
+   * the boughtList managing
+   *
+   * @param  {function} ShoppingListCheckOffService a singleton service for
+   *  data storing and sharing
+   *
+   */
   AlreadyBoughtController.$inject=['ShoppingListCheckOffService'];
   function AlreadyBoughtController(ShoppingListCheckOffService){
     var list = this;
-    list.items=[];
-    
+    list.items=ShoppingListCheckOffService.getItemsBought();
+
+
+
   };
+
+  function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var itemsToBuy = [];
+  var itemsBougth =[];
+
+    // I decided to fullfil my toBuyList using the object constructor
+  for (let i=1;i<6;i++){
+    let itemEntity = new Items('Cookie(s)',i);
+    itemsToBuy.push(itemEntity);
+  };
+
+  service.buyItem = function (itemIdex) {
+    itemsBougth.push(itemsToBuy[itemIdex]);
+    itemsToBuy.splice(itemIdex, 1);
+  };
+
+  service.getItemsToBuy = function () {
+    return itemsToBuy;
+  };
+
+  service.getItemsBought = function () {
+    return itemsBougth;
+  };
+
+};
+
+/**
+ * Items - the constructor to create items in itemsList
+ *
+ * @param  {string} name     a name for the item
+ * @param  {number} quantity quontity of items
+ * @return {object}          the resulting item object
+ */
+function Items(name,quantity) {
+  this.name=name;
+  this.quantity=quantity;
+};
+
 
 })();
